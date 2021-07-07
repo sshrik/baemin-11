127d7d97824235424729a7da9e6d854e7401220c
sshrik <chj878194@naver.com>
2021-07-07 13:21:38 +0900

    Convention 맞추기용 삭제.

--- routes/login.js
+++ routes/login.js
@@ -6,13 +6,4 @@ router.get('/', function(req, res, next) {
     res.render("screens/login");
 });
 
-const signUpApi = require("../api/signUp.js");
-
-router.post('/checkDuplicate', function(req, res, next) {
-    if(signUpApi(req.body.id)) {
-        // 중복에 관련된 로직 실행.
-        res.send("DENY");
-    }
-});
-
 module.exports = router;
