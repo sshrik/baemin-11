96c7a11f66405176295d064f1dd42dd707c67733
admin <admin@adminui-MacBookPro.local>
2021-07-07 13:20:34 +0900

    express에 폴더 구조 추가
    - models : lib에 있는 db basic api를 사용한 더 많은 기능 제공
    - routes : rest api 제공
    - lib : Database가 들어있는 라이브러리 폴더
    - services : model의 코드를 사용하여 routes에서 사용할 business api 제공
    - middleware : 좀 더 util적인 내용 (validate, auth etc)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("./middlewares/index");
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -19,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
