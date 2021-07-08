ad3494975bae9410426e1525897f931b79fa67ef
sshrik <chj878194@naver.com>
2021-07-09 00:06:34 +0900

    Error Handler 추가 및 중복 체크 API 완성.

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,39 +2,70 @@ const db = require("../lib/Database.js");
 
 const userDB = "userInfor.json";
 
-function checkDuplicate(id, pw, next) {
-  db.checkDuplicate(userDB, (item) => {
-    return item.id === id;
-  })
-  .then((isDuplicate) => {
-    if(isDuplicate) {
-      next({
-        result: "fail",
-        msg: "이미 존재하는 ID입니다."
-      }, false);
-    }
-    else {
-      db.insert(
-        userDB, {
-        id: id, pw: pw}, 
-        {autoKey: true}
-      )
-      .then(() => {
-        next({
-          result: "sucess",
-          msg: "회원가입 성공!"
-        }, true);
-      });
-    }
-  })
-  .catch((err) => {
+function errorHandler(nowRun, next) {
+  try{
+    nowRun();
+  }
+  catch(err) {
     next({
       result: "fail",
       msg: "Internal Server Error"
     }, false);
-  });
+  }
+}
+
+function addTo(id, pw, next) {
+  errorHandler(() => {
+    db.checkDuplicate(userDB, (item) => {
+      return item.id === id;
+    })
+    .then((isDuplicate) => {
+      if(isDuplicate) {
+        next({
+          result: "fail",
+          msg: "이미 존재하는 ID입니다."
+        }, false);
+      }
+      else {
+        db.insert(
+          userDB, {
+          id: id, pw: pw}, 
+          {autoKey: true}
+        )
+        .then(() => {
+          next({
+            result: "sucess",
+            msg: "회원가입 성공!"
+          }, true);
+        });
+      }
+    })
+  }, next);
+}
+
+function checkDuplicate(id, next) {
+  errorHandler(() => {
+    db.checkDuplicate(userDB, (item) => {
+      return item.id === id;
+    })
+    .then((isDuplicate) => {
+      if(isDuplicate) {
+        next({
+          result: false,
+          msg: "이미 존재하는 ID입니다."
+        }, true);
+      }
+      else {
+        next({
+          result: true,
+          msg: "존재하지 않는 ID입니다."
+        }, true);
+      }
+    })
+  },next);
 }
 
 module.exports = {
-  checkDuplicate:checkDuplicate
+  addTo: addTo,
+  checkDuplicate: checkDuplicate
 }
--- routes/api.js
+++ routes/api.js
@@ -2,18 +2,30 @@ const express = require('express');
 const router = express.Router();
 const auth = require("../middlewares/auth.js");
 
+function sendData(response, result, resTF) {
+  if(resTF) {
+    response.statusCode = 200;
+  }
+  else {
+    response.statusCode = 400;
+  }
+  response.send(JSON.stringify(result));
+}
+
 router.post('/signIn', function(req, res, next) {
   const id = req.body.id;
   const pw = req.body.pw;
 
-  auth.checkDuplicate(id, pw, (result, resTF) => {
-    if(resTF) {
-      res.statusCode = 200;
-    }
-    else {
-      res.statusCode = 400;
-    }
-    res.send(JSON.stringify(result));
+  auth.addTo(id, pw, (result, resTF) => {
+    sendData(res, result, resTF) 
+  });
+});
+
+router.post('/checkDup', function(req, res, next) {
+  const id = req.body.id;
+
+  auth.checkDuplicate(id, (result, resTF) => {
+    sendData(res, result, resTF) 
   });
 });
 
