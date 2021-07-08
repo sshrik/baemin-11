fd1a4d9c047844788354f247f5d511a7d84ccf5a
Juyoung Park <juyoung7018.park@gmail.com>
2021-07-08 20:33:31 +0900

    공용으로 사용할 validator 함수들 추가

---
+++ assets/utils/validator.js
@@ -0,0 +1,6 @@
+const REGEX = {
+  EMAIL: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+}
+
+export const email = (str) => REGEX.EMAIL.test(str);
+export const password = str => Boolean(str);
